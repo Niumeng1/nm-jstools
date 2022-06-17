268b7cdae71bc175fe21b80d3034be7b3b8b8dba
dci-nm <875347807@qq.com>
2022-06-17 17:38:12 +0800

    1.optimize datamake

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
  * @Description: 
  * @Author: 牛猛
  * @Date: 2022-06-15 16:37:27
- * @LastEditTime: 2022-06-17 17:29:49
+ * @LastEditTime: 2022-06-17 17:37:56
  * @LastEditors: nm
  * @FilePath: \nm-jstools\src\index.js
  */
 import {getTypeOf,unique} from "./utils"
 import Validate from "./validare"
 
-import dataMake from "./data-make"
+import dataMake from "./data-make/index"
 import dateMake from "./date-make/index"
 export  {
   getTypeOf,
