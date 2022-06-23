c2a24bb60781bff82667c709bb86316cf8b59e72
dci-nm <875347807@qq.com>
2022-06-23 16:56:54 +0800

    1.add module mapbox dom

--- src/date-make/index.test.js
+++ src/date-make/index.test.js
@@ -2,11 +2,11 @@
  * @Description: 
  * @Author: 牛猛
  * @Date: 2022-06-17 15:58:26
- * @LastEditTime: 2022-06-17 17:28:39
+ * @LastEditTime: 2022-06-23 16:55:08
  * @LastEditors: nm
  * @FilePath: \nm-jstools\src\date-make\index.test.js
  */
 import dateUtil from "./index";
 test('get current month 01 - current day',()=>{
-    expect(dateUtil.getTime()).toEqual(['2022-06-01','2022-06-17']);
+    expect(dateUtil.getTime()).toEqual(['2022-06-01','2022-06-23']);
 })
